6ae72d16095c6673b96436af6947806d1cc9a8c3
Bubfusion <thebubfusion@gmail.com>
2019-01-11 12:22:11 -0500

    ughhhhh

--- battleship.cpp
+++ battleship.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void boardOutput(char output[10][10]);
+void boardOutput(char output[][10]);
 
 int main()
 {
@@ -11,6 +11,9 @@ srand(time(NULL));
 char userBoard[10][10];
 char userAttack[10][10];
 char aiBoard[10][10];
+char direction;
+int down;
+int across;
 
 for (int i = 0; i < 10; i++)
 		{
@@ -22,15 +25,39 @@ for (int i = 0; i < 10; i++)
 			}
     }
 
-    cout << boardOutput(userBoard[][]) << endl;
+		cout << "This is your board!" << endl;
+    boardOutput(userBoard);
+
+		cout << "This is a destroyer and will take up 2 tiles" << endl << "##" << endl;
+		cout << "Please input either vertical or horizontal(h,v): ";
+		cin >> direction;
+
+			cout << "How many spaces down would you like to move the destroyer?" << endl;
+			cin >> down;
+			cout << "How mant spaces across would you like to go?" << endl;
+			cin >> across;
+
+			if(direction == 'v' && down < 9 && across < 10)
+			{
+				userBoard[down][across] = '#';
+				userBoard[down+1][across] = '#';
+			}
+			if(direction == 'h' && down < 10 && across < 9)
+			{
+				userBoard[down][across] = '#';
+				userBoard[down][across+1] = '#';
+			}
+			boardOutput(userBoard);
+
+
 return 0;
 }
 
-void boardOutput(char output[10][10])
+void boardOutput(char output[][10])
 {
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i < 10; i++)
   {
-    for(int j = 0; j < 5; j++)
+    for(int j = 0; j < 10; j++)
     {
       cout << setw(2) << output[i][j]; //loop used to output the prize board
     }
